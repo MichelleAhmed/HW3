7d0ab8eeca1c03df1918faf625f254f4c55b643b
michelle <16100096@lums.edu.pk>
2015-03-24 14:46:25 -0300

    hw3

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,9 @@
+
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :rating, :description, :release_date
+	attr_accessible :title, :rating, :description, :release_date
+  	@all_ratings = Array.new(['G','PG','PG-13','R'])
+  	def self.ratings
+    	return @all_ratings    
+  	end
+  
 end
